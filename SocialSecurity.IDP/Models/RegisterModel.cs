43cf1322146593d1be120227513d940c30466247
Kalash <kalash.t@mishainfotech.com>
2025-05-08 15:09:33 +0530

    Update RegisterModel.cs

--- SocialSecurity.IDP/Models/RegisterModel.cs
+++ SocialSecurity.IDP/Models/RegisterModel.cs
@@ -2,6 +2,7 @@ namespace SocialSecurity.IDP.Models
 {
     public class RegisterModel
     {
+        //cmt
         public required string Email { get; set; }
         public required string Password { get; set; }
     }
